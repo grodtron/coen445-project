e372ac4e878d246256f538b1e8500cf63152bbb0
Jacky <jacky@jacky-VirtualBox.(none)>
2014-11-23 22:26:35 -0400

    Updated client.py to use functions

--- src/coen445/project/client/client.py
+++ src/coen445/project/client/client.py
@@ -2,8 +2,16 @@ import socket
 
 from sys import argv
 
-localport = int(argv[1])
-username = argv[2]
+try:
+    localport = int(argv[1])
+    username = argv[2]
+except:
+    try:
+        localport = int(raw_input("Please enter a valid port number: "))
+        username = raw_input("Please enter a valid username: ")
+    except:
+        print "Unspecified error. Exiting..."
+        exit()
 
 udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,31 +30,22 @@ serversock.listen(1)
 address = ('localhost', 12358)
 
 register = b'\x04\x00' + chr(len(username)) + username + b'\x7f\x00\x00\x01' + chr(int(hex(localport)[2:4],16)) + chr(int(hex(localport)[4:6],16))
-offer    = b'\x06\x00' + chr(len(username)) + username + b'\x7f\x00\x00\x01\tSome Shit\x00\n'
 
-udpsock.sendto(register, address)
-udpsock.recv(1024)
+def register_client(register, address):
+    udpsock.sendto(register, address)
+    udpsock.recv(1024)
+    serversock.settimeout(10)
+    return serversock.accept()
 
-conn, client = serversock.accept()
+conn, client = register_client(register, address)
 
-udpsock.sendto(offer, address)
-offerconf = udpsock.recv(1024)
+def offer_item(item):
+    offer    = b'\x06\x00' + chr(len(username)) + username + b'\x7f\x00\x00\x01\t'+item+'\x00\n'
+    udpsock.sendto(offer, address)
+    offerconf = udpsock.recv(1024)
+    return 256*ord(offerconf[2]) + ord(offerconf[3])
 
-#port = ((0xff & ord(offerconf[2])) << 8) | (0xff & ord(offerconf[3]))
-port = 256*ord(offerconf[2]) + ord(offerconf[3])
-
-tcpsock.connect(('localhost', port))
-
-tcpsock.send(b"\x00\0\0\0\0\0\0")
-#resp = tcpsock.recv(100)
-
-#print (repr(resp))
+port = offer_item("abcdefghijklmnopqrstuvwxyz")
 
 print ("receiving from server")
-print (repr(conn.recv(100)))
-
-while True:
-	try:
-		eval(raw_input(">>> "))
-	except Exception as e:
-		print(e)
+#print (repr(conn.recv(100)))
