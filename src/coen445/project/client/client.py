1c624e99afc06117efb00e88128cdcc577ee6e40
Jacky <jacky@jacky-VirtualBox.(none)>
2014-11-23 23:42:36 -0400

    client again

--- src/coen445/project/client/client.py
+++ src/coen445/project/client/client.py
@@ -26,26 +26,51 @@ serversock.bind(('localhost', localport))
 
 serversock.listen(1)
 
-
 address = ('localhost', 12358)
 
-register = b'\x04\x00' + chr(len(username)) + username + b'\x7f\x00\x00\x01' + chr(int(hex(localport)[2:4],16)) + chr(int(hex(localport)[4:6],16))
+auction = []
+# example of adding an item 
+    # car = {"port" : 14533, "current_bid": 10, "client_bid": 10}
+    # auction.append(car)
 
-def register_client(register, address):
+def register_client():
+    register = b'\x04\x00' + chr(len(username)) + username + b'\x7f\x00\x00\x01' + chr(int(hex(localport)[2:4],16)) + chr(int(hex(localport)[4:6],16))
     udpsock.sendto(register, address)
     udpsock.recv(1024)
     serversock.settimeout(10)
     return serversock.accept()
 
-conn, client = register_client(register, address)
+def deregister_client():
+    register = b'\x02\x00' + chr(len(username)) + username + b'\x7f\x00\x00\x01' + chr(int(hex(localport)[2:4],16)) + chr(int(hex(localport)[4:6],16))
+    udpsock.sendto(register, address)
+    udpsock.recv(1024)
+    serversock.settimeout(10)
+    try:
+        result = serversock.accept()
+        print result
+    except:
+        print "Server took too long to respond. Force quiting..."
+    # TO-DO: CONFIRM THAT SERVER ACKNOWLEDGES DEREGISTER REQUEST
+    exit()
 
-def offer_item(item):
-    offer    = b'\x06\x00' + chr(len(username)) + username + b'\x7f\x00\x00\x01\t'+item+'\x00\n'
+def offer_item(item,price):
+    offer    = b'\x06\x00' + chr(len(username)) + username + b'\x7f\x00\x00\x01\t'+item+chr(int(hex(price),16))+'\n'
     udpsock.sendto(offer, address)
     offerconf = udpsock.recv(1024)
-    return 256*ord(offerconf[2]) + ord(offerconf[3])
+    port = 256*ord(offerconf[2]) + ord(offerconf[3])
+    return port
 
-port = offer_item("abcdefghijklmnopqrstuvwxyz")
+def bid_item(item):
+    # 
+    print "Place holder"
 
-print ("receiving from server")
+try:
+    conn, client = register_client()
+except:
+    print "Could not connect to server. Please try another port number."
+    exit()
+
+port = offer_item("abcdefghijklmnopqrstuvwxyz",1)
+
+#print ("receiving from server")
 #print (repr(conn.recv(100)))
