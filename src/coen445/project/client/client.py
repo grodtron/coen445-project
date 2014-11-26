4de5fb7210de3beb50d031b943be79833b2161f6
Jacky <jacky@jacky-VirtualBox.(none)>
2014-11-26 15:21:59 -0400

    client

--- src/coen445/project/client/client.py
+++ src/coen445/project/client/client.py
@@ -1,6 +1,8 @@
 import socket
 
 from sys import argv
+from time import sleep
+from multiprocessing import Thread, Lock
 
 try:
     localport = int(argv[1])
@@ -12,6 +14,10 @@ except:
     except:
         print "Unspecified error. Exiting..."
         exit()
+
+if localport > 0xFFFF:
+    print "Bad port. Please try again. Exiting..."
+    exit()
 
 udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -34,9 +40,16 @@ auction = []
     # auction.append(car)
 
 def register_client():
-    register = b'\x04\x00' + chr(len(username)) + username + b'\x7f\x00\x00\x01' + chr(int(hex(localport)[2:4],16)) + chr(int(hex(localport)[4:6],16))
+    register = b'\x04\x00' + chr(len(username)) + username + b'\x7f\x00\x00\x01' + chr(int(hex(localport)[2:][-4:-2],16)) + chr(int(hex(localport)[2:][-2:],16))
     udpsock.sendto(register, address)
-    udpsock.recv(1024)
+    udpsock.settimeout(10)
+    try:
+        response = udpsock.recv(1024)
+    except Exception as omg_gordon_fine:
+        print omg_gordon_fine
+        return 0,0
+    if ord(response[0]) != 3:
+        print 0,0
     serversock.settimeout(10)
     return serversock.accept()
 
@@ -45,6 +58,7 @@ def deregister_client():
     udpsock.sendto(register, address)
     udpsock.recv(1024)
     serversock.settimeout(10)
+
     try:
         result = serversock.accept()
         print result
@@ -54,7 +68,7 @@ def deregister_client():
     exit()
 
 def offer_item(item,price):
-    offer    = b'\x06\x00' + chr(len(username)) + username + b'\x7f\x00\x00\x01\t'+item+chr(int(hex(price),16))+'\n'
+    offer    = b'\x06\x00' + chr(len(username)) + username + b'\x7f\x00\x00\x01'+ chr(len(item)) +item+chr(int(hex(price),16))+'\n'
     udpsock.sendto(offer, address)
     offerconf = udpsock.recv(1024)
     port = 256*ord(offerconf[2]) + ord(offerconf[3])
@@ -65,12 +79,23 @@ def bid_item(item):
     print "Place holder"
 
 try:
+    # client is the server's response variable 'address'
     conn, client = register_client()
-except:
+except Exception as e:
     print "Could not connect to server. Please try another port number."
+    print e
     exit()
 
-port = offer_item("abcdefghijklmnopqrstuvwxyz",1)
+while (conn == 0) and (client == 0):
+    try:
+        # client is the server's response variable 'address'
+        print "Could not connect to server. Attempting again..."
+        sleep(2)
+        conn, client = register_client()
+    except Exception as e:
+        print "Could not connect to server. Please try another port number."
+        print e
+        exit()
 
 #print ("receiving from server")
 #print (repr(conn.recv(100)))
